46c5ca34e549098c6fee47ae01a1ef214a83dfee
brianburger <254530-brianburger@users.noreply.replit.com>
2023-08-21 21:18:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+/* Multiline comment
+This is our first program
+System.out.println
+*/
+
+
+class Main {
+  public static void main(String[] args) {
+    //Two versions of print
+    System.out.println("Hello world!");
+    System.out.println("AP COMPUTER SCIENCE");
+    System.out.print("7th Period ");//This version does not return to the next line after the print
+    System.out.print("PR ");
+    System.out.print("USA");
+  }
+}
